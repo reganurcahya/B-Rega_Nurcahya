3fd67d2b7f869d1e56140867e4f212e143d3fcd9
reganurcahya <115162758+reganurcahya@users.noreply.github.com>
2024-11-08 01:41:02 +0700

    praktik dan tugas

---
+++ Javascript Function/praktik.js
@@ -0,0 +1,14 @@
+//function sayHallo(person){
+//    console.log('Hi, ${person}');
+//}
+
+//sayHallo('dony')
+//sayHallo('aulia')
+
+//function findAngka(x,y){
+//    return x*y;
+//}
+
+function sayhallo(person){
+    console.log("Hello")
+}
