337d81e3ce538cc53094784bcc055cec38290cb4
reganurcahya <reganurcahya1@gmail.com>
2024-11-07 23:34:50 +0700

    first commit

---
+++ Javascript Introduction/belajar.js
@@ -0,0 +1,58 @@
+// Using “script” tag on body
+alert("this is an alert method")
+confirm("you want know me more")
+prompt("how old are u?")
+
+// Javascript Variables const
+const x = 5;
+const y = 6;
+const z = x + y;
+document.getElementById("demo").innerHTML = "The value of Z is:" + z;
+
+/* Javascript Variables let
+let x = 5;
+let y = 6;
+let z = x + y;
+document.getElementById("demo").innerHTML = "The value of Z is:" + z; */
+
+/* Javascript Variables let
+var x = 5;
+var y = 6;
+var z = x + y;
+document.getElementById("demo").innerHTML = "The value of Z is:" + z; */
+
+/* Javascript Variables mixed varibale
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2 ;
+document.getElementById("demo").innerHTML = "The total is:" + total; */
+
+/* Javascript Variables var string
+const pi = 3.14;
+let person = "john Doe";
+let answer = "yes I am!";
+document.getElementById("demo").innerHTML = pi + "<br>" + person +"<br>" + answer; */
+
+/* Javascript Variables let, var, const
+// Menggunakan var
+var x = 10;
+document.write("<p>Nilai x (dengan var): " + x + "</p>");
+
+// Menggunakan Let
+let y = 20;
+document.write("<p>Nilai y (dengan var): " + y + "</p>");
+
+// Menggunakan const
+const z = 30 
+document.write("<p>Nilai z (dengan var): " + z + "</p>");
+
+// Cobalah mengubah nilai variable
+
+x = 15; // Ok
+y = 25; // Ok
+// z = 35; // Error! karena z adalah konstanta
+
+document.write("<p>Setelah diubah:</p>");
+document.write("<p>Nilai x: "+ x +"</p>")
+document.write("<p>Nilai y: "+ y +"</p>")
+document.write("<p>Nilai z: "+ z +"</p>") */
