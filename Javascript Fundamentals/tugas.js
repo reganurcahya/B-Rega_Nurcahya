a84c2e197275674ed0e59390714af576053868da
reganurcahya <reganurcahya1@gmail.com>
2024-11-08 01:24:01 +0700

    praktik

---
+++ Javascript Fundamentals/tugas.js
@@ -0,0 +1,10 @@
+
+    let nama = "Rega Nurcahya";
+    let umur = 21;
+    let mahasiswa = "STT terpadu Nurul fikri";
+
+    // Menampilkan pesan menggunakan alert
+    alert("Halo, nama saya " + nama + ". Saya berusia " + umur + " tahun. saya adalah mahasiswa dari " + mahasiswa);
+
+    // Menampilkan pesan ke konsol browser
+    console.log("Ini adalah pesan yang hanya terlihat di konsol browser.");
