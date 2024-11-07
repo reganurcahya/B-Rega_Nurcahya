0421079d6a36791bbced774e6ff0955db0b0bec5
reganurcahya <115162758+reganurcahya@users.noreply.github.com>
2024-11-08 01:41:33 +0700

    praktik dan tugas

---
+++ Javascript Fundamentals/praktik.js
@@ -0,0 +1,12 @@
+//let x = "Celerates"
+//let y = 2024;
+
+//alert (x+y);
+//console.log(x+y);
+
+//let x = ["owl","cat"]
+// x[0]="bird";
+// console.log(x);
+
+let x = {firstname:'Rega' , lastname:'Nurcahya'};
+console.log(x);
