17ba3f13794551d5d64852163040e0306b30cf79
reganurcahya <115162758+reganurcahya@users.noreply.github.com>
2024-11-08 11:37:13 +0700

    Praktik dan tugas

---
+++ Javascript String and Array/script.js
@@ -0,0 +1,13 @@
+let antrian = ["ray", "fiki", "fadhilla", "farah"];
+
+    antrian.push("nabila");
+    antrian.push("maza", "elsi");
+
+antrian.pop(); 
+    antrian.shift(); 
+    antrian.shift(); 
+    antrian.unshift("tomi");
+
+    let nyerobot = antrian.slice();
+
+    console.log("Antrian akhir:", nyerobot);
