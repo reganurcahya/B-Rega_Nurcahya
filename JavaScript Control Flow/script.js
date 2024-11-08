b87be52fe1e2e3a0a74270a9b37b7ec9acf45d21
reganurcahya <115162758+reganurcahya@users.noreply.github.com>
2024-11-08 11:37:34 +0700

    Praktik dan Tugas

---
+++ JavaScript Control Flow/script.js
@@ -0,0 +1,15 @@
+// 1. Percobaan untuk mempraktikan pengulangan pada js
+for (let i = 0; i < 6; i++) {
+    console.log("Da ba dee da ba daa");
+}
+
+// 2.  membuat percobaan perulangan dengna perhitungan
+for (let i = 25; i >= 0; i -= 5) {
+    console.log(i);
+}
+
+// 3. latihan untuk mengaplikasikan perulangan dengan menggunakan array string.
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"];
+for (let i = 0; i < people.length; i++) {
+    console.log(people[i].toUpperCase());
+}
